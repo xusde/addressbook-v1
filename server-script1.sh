6ae5391f6594b9d6169518ce68acc4fd1f902dc2
XuSDE <61101213+xusde@users.noreply.github.com>
2026-06-02 13:19:09 -0400

    Update server-script1.sh for Ubuntu compatibility

---
+++ server-script1.sh
@@ -0,0 +1,17 @@
+# sudo install jre, and maven, git is already there in ubuntu
+sudo apt update
+sudo apt install fontconfig openjdk-21-jre
+sudo apt install mvn
+
+
+if [ -d "addressbook-v1" ]
+then
+  echo "repo is cloned and exists"
+  cd /home/ubuntu/addressbook-v1
+  git pull origin master
+else
+  git clone https://github.com/preethid/addressbook-v1.git
+fi
+
+cd /home/ubuntu/addressbook-v1
+mvn compile
